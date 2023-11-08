188adc39f4fba0f9fbdb31d0baa1a96e9bb66ce4
vandat3012 <luongvandat301220@gmail.com>
2023-11-08 16:04:57 +0700

    update bài tập

---
+++ hien_thi_dau_ra.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert("xin chào");
+}
+function showMessage1(){
+    document.getElementById('mess').innerHTML='Xin chào 1';
+}
+function showMessage3(){
+    document.write("Xin chào3");
+}
+function showMessage5(){
+    console.log("Xin chào5");
+}
